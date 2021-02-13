c61c8606d7acae9d70ef870a07db50e70829bba3
raisedwizardry <lifebloodasmr@gmail.com>
2021-02-13 10:36:58 -0500

    update to import the local npm packages

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { IntroductionModule } from 'introduction';
+import { SayingHelloModule } from 'saying-hello';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -9,6 +11,8 @@ import { AppComponent } from './app.component';
     AppComponent
   ],
   imports: [
+    IntroductionModule,
+    SayingHelloModule,
     BrowserModule,
     AppRoutingModule
   ],
